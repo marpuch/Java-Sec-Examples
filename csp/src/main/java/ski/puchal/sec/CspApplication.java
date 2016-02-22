ae9c756ec023a79334c60ac87096ef20dd48e918
Marek Puchalski <marpuch@gmail.com>
2016-02-22 23:12:30 +0100

    CSP examples added

---
+++ csp/src/main/java/ski/puchal/sec/CspApplication.java
@@ -0,0 +1,12 @@
+package ski.puchal.sec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CspApplication.class, args);
+	}
+}
---
+++ csp/src/main/resources/static/js/own.js
@@ -0,0 +1,6 @@
+var script = function() {
+	var inline3 = document.getElementById("inline3"),
+        inline_s3 = document.getElementById("inline_s3");
+	inline3.className = "list-group-item list-group-item-success";
+	inline_s3.innerHTML = "Ok";
+}
